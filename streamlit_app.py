1c52a828e9031bd141ede4eab56a554be9d73860
Bill Gallagher <billga@users.noreply.github.com>
2025-05-16 06:26:35 +0000

    Finished the bee

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import twl
+import random
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Eldrow!")
+
+words = [*twl.iterator()]
+word5 = [w for w in words if len(w)==5]
+# st.write(random.choice(word5))
+bee = st.text_input(":bee:", help="center letter first, 7 letters total")
+
+if len(bee)==7:
+    candidates = sorted((w for w in words if set(w).issubset(bee) and bee[0] in w), key=len, reverse=True)
+    pangrams = [w for w in candidates if set(bee).issubset(w)]
+    st.badge(f"{len(candidates)} / {len(pangrams)}", color="blue")
+    # st.markdown(pangrams)
+    for w in candidates:
+        st.write(f"{w+' <-:100:' if set(bee).issubset(w) else w}")
+        
